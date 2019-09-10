89fc572d54db784fb99ad51f685cd2c45330e317
Ben Owen <LordBenjamin@users.noreply.github.com>
2019-09-10 08:25:07 +0100

    Move into subfolder

---
+++ 01-glutin/desktop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Desktop {
+    internal class Program {
+        public static void Main(string[] args) {
+            Form form = new Form();
+            form.Text = "Sharp and Rusty";
+
+            var table = new TableLayoutPanel();
+            table.Dock = DockStyle.Fill;
+            table.ColumnCount = 1;
+            table.RowCount= 2;
+
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            table.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+
+            var renderTarget = new Control();
+            renderTarget.Dock = DockStyle.Fill;
+            table.Controls.Add(renderTarget, 0, 1);
+
+            var fpsLabel = new Label();
+            table.Controls.Add(fpsLabel, 0, 0);
+
+            form.Controls.Add(table);
+
+            using (Renderer renderer = new Renderer(renderTarget.Handle)) {
+                renderTarget.Paint += (sender, e) => {
+                    Stopwatch sw = Stopwatch.StartNew();
+                    renderer.Draw();
+                    fpsLabel.Text = Math.Round(1.0 / sw.Elapsed.TotalSeconds, 2) + " FPS";
+                };
+
+                renderTarget.Resize += (sender, e) => renderer.Resize(renderTarget.Size);
+
+                Application.Run(form);
+            }
+        }
+    }
+}
