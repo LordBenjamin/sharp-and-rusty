b39f41844c262fb8e2c47310dd6cf31a3fe44457
Ben Owen <LordBenjamin@users.noreply.github.com>
2019-09-16 20:51:30 +0100

    cargo fmt

--- 05-rust-subclass-window/rust/src/lib.rs
+++ 05-rust-subclass-window/rust/src/lib.rs
@@ -1,21 +1,28 @@
 extern crate winapi;
 
-use winapi::shared::basetsd::{UINT_PTR,  DWORD_PTR };
+use winapi::shared::basetsd::{DWORD_PTR, UINT_PTR};
 use winapi::shared::minwindef::{LPARAM, LRESULT, UINT, WPARAM};
 use winapi::shared::windef::HWND;
-use winapi::um::winuser::{ WM_DESTROY };
-use winapi::um::commctrl::{SetWindowSubclass, DefSubclassProc};
+use winapi::um::commctrl::{DefSubclassProc, SetWindowSubclass};
+use winapi::um::winuser::WM_DESTROY;
 
 #[no_mangle]
 pub unsafe extern "C" fn subclass(hwnd: HWND) {
     SetWindowSubclass(hwnd, Some(wnd_proc), 1, 0);
 }
 
-unsafe extern "system" fn wnd_proc(hwnd: HWND, message: UINT, wparam: WPARAM, lparam: LPARAM, _u_id_subclass: UINT_PTR, _dw_ref_data: DWORD_PTR) -> LRESULT {
+unsafe extern "system" fn wnd_proc(
+    hwnd: HWND,
+    message: UINT,
+    wparam: WPARAM,
+    lparam: LPARAM,
+    _u_id_subclass: UINT_PTR,
+    _dw_ref_data: DWORD_PTR,
+) -> LRESULT {
     match message {
         WM_DESTROY => println!("window destroyed!"),
-        _ => ()
+        _ => (),
     };
-    
+
     DefSubclassProc(hwnd, message, wparam, lparam)
 }
